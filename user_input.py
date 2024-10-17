8bc7b6b2aa2661fbff9adbd60b2aa900c84613b7
Sam  Ntlombe <106554107+Sam-366@users.noreply.github.com>
2024-10-17 19:36:30 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,21 @@
+# Ask user for name
+
+name = input("Enter name:")
+
+print("My name is: " + name)
+
+# Enter age
+
+age = input("Enter age")
+
+print("My age is: " + age)
+
+# Enter location
+
+location = input("Enter location")
+
+print("My location is: " + location)
+
+# Personalised message
+
+print("Hello, my name is: " + name "and my age is: " + age "and my location is: " + location)
